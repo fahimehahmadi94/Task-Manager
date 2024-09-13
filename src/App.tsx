1e47a892454de620ff1c2868121dffc6cefaef50
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2024-09-13 16:57:41 +0330

    install tailwindui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import Home from "./pages/Home/Home";
-
+import Dashboard from './Dashboard/Dashboard';
 function App() {
     return (
         <div>
-            <Home/>
+            <Dashboard />
         </div>
     );
 }
---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1 className="text-green-500">
+                Dashboard
+            </h1>
+        </div>
+    );
+}
+
+export default Dashboard;
