e492c45501bb40d18a49671a6a13cb8c50bf76d2
Fahimeh Ahmadi <fahimehahmadi.fafa@gmail.com>
2024-09-21 20:25:45 +0330

    get all Task

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,43 +1,37 @@
 import { useEffect, useState } from "react";
 import Column from "../Column/Column";
-import { BoardInterface } from "../../models/interfaces/Board.interfacse";
+import { BoardInterface } from "../../models/interfaces/Board.interface";
 import axios from "axios";
 import SkeletonBoard from "./SkeletonBoard";
 
 const Board = () => {
-    const columns = ["Todo", "Doing", "Done"];
-
-    const [data, setData] = useState<BoardInterface[]>([])
+    const [data, setData] = useState<BoardInterface[]>([]);
 
     useEffect(() => {
-        axios.get('https://task-manager-d6149-default-rtdb.firebaseio.com/getAllTask.json')
+        axios.get("https://task-manager-d6149-default-rtdb.firebaseio.com/getAllTask.json")
             .then((res) => {
-                const fetchedData = res.data.data ? [res.data.data] : [];
+                const fetchedData = res.data ? res.data.data : [];
                 setData(fetchedData);
             })
-            .catch(err => {
-                console.log('Error:', err);
+            .catch((err) => {
+                console.log("Error:", err);
                 setData([]);
             });
     }, []);
 
     return (
         <div className="h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {
-                data && data.length > 0 ? (
-                    data.map((d, i) => (
-                        <Column key={i} columnName={d.title} />
-                    ))
-                ) : (
-                    <>
-                        <SkeletonBoard />
-                    </>
-                )
-            }
+            {data && data.length > 0 ? (
+                data.map((d, i) => (
+                    <Column key={i} columnName={d.title} />
+                ))
+            ) : (
+                <>
+                    <SkeletonBoard />
+                </>
+            )}
         </div>
     );
-
-}
+};
 
 export default Board;
-
